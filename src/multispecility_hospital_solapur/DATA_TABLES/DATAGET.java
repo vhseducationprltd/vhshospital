f73a84ccae0f03b2428c974b73545cba0911dca6
Hemant.wakade@truethic.in <Hemant.wakade@truethic.in>
2022-04-30 19:29:06 +0530

    first commit after fresh project

---
+++ src/multispecility_hospital_solapur/DATA_TABLES/DATAGET.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multispecility_hospital_solapur.DATA_TABLES;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JFrame;
+import javax.swing.table.DefaultTableModel;
+import multispecility_hospital_solapur.use.GetConnection;
+
+/**
+ *
+ * @author heman
+ */
+public class DATAGET {
+    
+    //   
+    
+         
+        
+    
+}
